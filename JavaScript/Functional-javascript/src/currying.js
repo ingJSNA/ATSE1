400d422dd2baa6cddb7b333c5ea3f76ab7d0f4c7
Juan Navarro <jsnavarroa@unal.edu.co>
2018-10-29 21:24:50 -0500

    Solve last exercises

--- JavaScript/Functional-javascript/src/currying.js
+++ JavaScript/Functional-javascript/src/currying.js
@@ -1,22 +1,13 @@
 function curryN(fn, n) {
     // SOLUTION GOES HERE
+
     if (!n) {
         n = fn.length; // arity
     }
 
-    if(!this.params){
-        this.params = [];
-    }
-
-    console.log('Params: ', params);
-    if(n === 0){
-        
-        return fn.apply(undefined, params);
-    }else{
-        return function(param){
-            params.push(param);
-            return curryN.bind(this, fn, --n);
-        }
+    return function curriedN(arg) {
+        if (n <= 1) return fn(arg)
+        return curryN(fn.bind(this, arg), n - 1)
     }
 }
 
@@ -29,7 +20,7 @@ function add3(one, two, three) {
 var curryC = curryN(add3)
 var curryB = curryC(1)
 var curryA = curryB(2)
-console.log(curryA(3)) // => 6
-console.log(curryA(10)) // => 13
+//console.log(curryA(3)) // => 6
+//console.log(curryA(10)) // => 13
 
-console.log(curryN(add3)(1)(2)(3)) // => 6
+//console.log(curryN(add3)(2)(3)(4)) // => 9
